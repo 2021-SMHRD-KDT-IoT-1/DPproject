aad1e40c6cd4883ea169830f153a8e8289866522
smhrd <smhrd@59.0.236.118>
2021-05-26 10:32:40 +0900

    강산 - 고장신고 기능 구현

--- DPproject/src/main/java/com/model/reportDAO.java
+++ DPproject/src/main/java/com/model/reportDAO.java
@@ -1,5 +1,71 @@
 package com.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class reportDAO {
+	PreparedStatement psmt = null;
+	Connection conn = null;
+	ResultSet rs = null;
+	int cnt = 0;
+
+	public void conn() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String db_url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String db_id = "hr";
+			String db_pw = "hr";
+
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	public int upload(reportDTO dto) {
+		conn();
+				
+		try {
+			String sql = "insert into report values(report_snum.nextval, ?, ?, ?, ?, sysdate)";
+
+			psmt = conn.prepareCall(sql);
+			
+			psmt.setString(1, dto.getReport_name());
+			psmt.setString(2, dto.getReport_tel());
+			psmt.setString(3, dto.getReport_title());
+			psmt.setString(4, dto.getReport_content());
+			
+			cnt = psmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+
+		return cnt;
+	}
 
 }
