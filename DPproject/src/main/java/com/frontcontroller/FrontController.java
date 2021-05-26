d0a50fa4f16cc52ae8f9ccd7d4340c87d7fcb1fb
SMHRD <SMHRD@220.80.88.121>
2021-05-26 12:50:03 +0900

    로그그그그그그그그그

--- DPproject/src/main/java/com/frontcontroller/FrontController.java
+++ DPproject/src/main/java/com/frontcontroller/FrontController.java
@@ -1,17 +1,50 @@
 package com.frontcontroller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.command.Command;
+import com.controller.JoinServiceCon;
+
 @WebServlet("*.do")
 public class FrontController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		request.setCharacterEncoding("EUC-KR");
 		
+		System.out.println("frontcontroller");
+		
+		//getURI 어떤 jsp/servlet에서 들어오는지
+		String reqURI = request.getRequestURI();
+		System.out.println("어디에서 왔는지 : " + reqURI);
+		
+		//프로젝트 이름 출력
+		String Path = request.getContextPath();
+		System.out.println("프로젝트 이름은 : " + Path);
+		
+		//servlet 이름만 출력하기
+		//문자열 자르는 함수
+		//subString(start) : 스타트 위치부터 끝까지 자르기
+		//substring(start, end) : 스타트부터 끝 위치까지 문자열 자르기
+		//path길이에 +1하는 이유 /를 빼기 위해
+		String resultURI = reqURI.substring(Path.length()+1);
+		System.out.println(resultURI);
+		
+		
+		//업캐스팅 하기 위해
+		Command command = null;
+		
+		if(resultURI.equals("JoinServiceCon.do")) {
+			command = new JoinServiceCon();	
+		}
+		
+		String moveURL = command.execute(request, response);
+		response.sendRedirect(moveURL);
 	
 	}
 
